d53c2fbbf6c2f6d7b16a32a2d982ab668bf0282f
Joanny Prignol <joannyprignol@gmail.com>
2025-10-16 16:32:22 +0200

    commentaires
    Signed-off-by: Joanny Prignol <joannyprignol@gmail.com>

--- resources/js/pages/Passions.vue
+++ resources/js/pages/Passions.vue
@@ -2,7 +2,7 @@
   <div>Passions Page</div>
   <div v-for="passion in passions" :key="passion.id" class="mb-6">
     <h2 class="text-xl font-bold mb-2">{{ passion.name }}</h2>
-    <p class="mb-2">{{ passion.description }}</p>
+    <p v-html="passion.description" class="mb-2"/>
     <img v-if="passion.image" :src="passion.image" :alt="passion.name" class="w-full h-auto rounded" />
   </div>
 </template>
