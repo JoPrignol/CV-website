afff9cddb131de0a29fcde5ea9107e2db791d0df
Joanny Prignol <joannyprignol@gmail.com>
2025-10-21 20:31:12 +0200

    mise en place du changement de langue en front (en cours)
    Signed-off-by: Joanny Prignol <joannyprignol@gmail.com>

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\App;
+
+// Middleware permettant de définir la langue de l'application en fonction d'un cookie 'locale'
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $locale = $request->cookie('locale', 'fr'); // FR par défaut
+        App::setLocale($locale);
+
+        return $next($request);
+    }
+}
--- resources/js/pages/Passions.vue
+++ resources/js/pages/Passions.vue
@@ -1,8 +1,9 @@
 <template>
+  <LanguageSwitcher />
   <div>Passions Page</div>
   <div v-for="passion in passions" :key="passion.id" class="mb-6">
-    <h2 class="text-xl font-bold mb-2">{{ passion.name }}</h2>
-    <p v-html="passion.description" class="mb-2"/>
+    <h2 class="text-xl font-bold mb-2">{{ passion.name[currentLocale] }}</h2>
+    <p v-html="passion.description[currentLocale]" class="mb-2"/>
     <img v-if="passion.image" :src="passion.image" :alt="passion.name" class="w-full h-auto rounded" />
   </div>
 </template>
@@ -10,6 +11,18 @@
 <script setup lang="ts">
 import { defineProps } from 'vue';
 import { Passion } from '@/types/passion';
+import LanguageSwitcher from '@/components/custom/LanguageSwitcher.vue';
+import { ref } from 'vue';
+
+function getLocale(): string {
+  const match = document.cookie.match(/locale=(\w+)/)
+  return match ? match[1] : 'fr'
+}
+
+// TODO: Problèmes: lors du changement de langue, les textes ne changent pas.
+// + Créer un composable pour éviter de dupliquer le code dans chaque page
+
+const currentLocale = ref(getLocale())
 
 const props = defineProps<{
   passions: Passion[];
