675472df3d8cd5fc2e3c35a4902500ea3748adda
Joanny Prignol <joannyprignol@gmail.com>
2025-10-28 21:15:25 +0100

    création du squelette du composant navbar
    Signed-off-by: Joanny Prignol <joannyprignol@gmail.com>

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="w-full flex items-center px-4 py-4" :class="isHomePage ? 'justify-end' : 'justify-between'">
+    <a
+      v-if="!isHomePage"
+      class="text-xl font-bold" href="/"
+    >
+      <i class="fa-solid fa-arrow-left" style="color: #ffffff;" />
+      {{localHome}}
+    </a>
+    <LanguageSwitcher />
+  </div>
+</template>
+
+<script setup lang="ts">
+import LanguageSwitcher from './custom/LanguageSwitcher.vue';
+import { usePage } from '@inertiajs/vue3';
+
+const props = defineProps<{
+  locale: string;
+}>();
+
+const page = usePage();
+const isHomePage = page.url === '/';
+
+let localHome = 'HOME';
+
+if (props.locale === 'fr') {
+  localHome = 'ACCUEIL';
+} else {
+  localHome = 'HOME';
+}
+
+</script>
