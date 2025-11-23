2a1929787bedb42e1275118f6599787d02043d5d
Joanny Prignol <joannyprignol@gmail.com>
2025-11-23 16:31:23 +0100

    Style de la page projets
    Signed-off-by: Joanny Prignol <joannyprignol@gmail.com>

--- resources/js/pages/Projects.vue
+++ resources/js/pages/Projects.vue
@@ -6,13 +6,13 @@
       class="w-1/2 border border-white rounded-md p-8"
     >
       <div v-if="project.tags.length" class="mb-1 w-full flex justify-end">
-        <span
+        <button
           v-for="tag in project.tags"
           :key="tag.id"
-          class="inline-block bg-gray-200 text-gray-800 text-xs px-2 py-1 rounded mr-2"
+          class="inline-block font-medium text-sm px-2 py-1 rounded-full border mr-2 text-black bg-white hover:bg-black hover:text-white duration-150 ease-in-out"
         >
           {{ tag.name[locale] }}
-        </span>
+        </button>
       </div>
 
       <h2
@@ -30,31 +30,47 @@
         v-if="project.url"
         :href="project.url"
         target="_blank"
-        class="text-xl mt-4 flex gap-2 items-center"
+        class="text-xl mt-4 flex gap-2 items-center w-fit text-moving-rainbow mb-4 font-black"
       >
-        <i class="fa-solid fa-angles-right" style="color: #ffffff;" />
+        <i class="fa-solid fa-angles-right" />
         <span>{{projectLink}}</span>
       </a>
 
-
       <div class="my-2">
-        <img
-          :src="project.main_image_url"
-          :alt="project.title"
-          class="w-[15rem] aspect-auto mb-2"
-        />
-        <div
-          v-if="project.secondary_images_urls"
-          class="flex space-x-2 overflow-x-auto gap-8"
+        <Carousel
+          v-bind="carouselConfig"
+          :autoplay="3000"
+          :pause-autoplay-on-hover="true"
+          slide-effect="fade"
+          :mouseWheel="true"
         >
-          <img
+          <!-- Slide pour l'image principale -->
+          <Slide v-if="project.main_image_url">
+            <img
+              :src="project.main_image_url"
+              :alt="project.title"
+              class="w-full h-fit object-cover rounded-md"
+            />
+          </Slide>
+
+          <!-- Slides pour les images secondaires -->
+          <Slide
             v-for="(image, index) in project.secondary_images_urls"
             :key="index"
-            :src="image"
-            :alt="`${project.title} image ${index + 1}`"
-            class="h-[10rem] aspect-auto"
-          />
-        </div>
+          >
+            <img
+              :src="image"
+              :alt="`${project.title} image ${index + 1}`"
+              class="w-full h-fit object-cover rounded-md"
+            />
+          </Slide>
+
+          <!-- Navigation et pagination -->
+          <!-- <template #addons>
+            <Navigation />
+            <Pagination />
+          </template> -->
+        </Carousel>
       </div>
 
 
@@ -63,7 +79,17 @@
 </template>
 
 <script setup lang="ts">
-import { Project } from '@/types/project';
+  import { Project } from '@/types/project';
+
+  import 'vue3-carousel/carousel.css'
+  import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
+  const carouselConfig = {
+    itemsToShow: 1,
+    wrapAround: true, // Boucle infinie
+    transition: 3000, // Durée de la transition en ms
+    snapAlign: 'start', // Alignement des slides
+  }
 
   const props = defineProps<{
     projects: Project[];
@@ -84,3 +110,24 @@ import { Project } from '@/types/project';
     }
   }
 </script>
+
+<style scoped>
+.text-moving-rainbow {
+  color: white;
+  background: linear-gradient(90deg, #00C0FF 0%, #FFCF00 49%, #FC4F4F 80%, #00C0FF 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+}
+
+.text-moving-rainbow:hover {
+  color: transparent;
+  transition: 500ms ease;
+  animation:slidebg 10s linear infinite;
+}
+
+@keyframes slidebg {
+  to {
+    background-position:20vw;
+  }
+}
+</style>
