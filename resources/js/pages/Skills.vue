a247dec0acdfdeeb6134a9736094055da7f07799
Joanny Prignol <joannyprignol@gmail.com>
2025-12-04 22:25:35 +0100

    modification du rendu des jauges de la page skills
    Signed-off-by: Joanny Prignol <joannyprignol@gmail.com>

--- resources/js/pages/Skills.vue
+++ resources/js/pages/Skills.vue
@@ -5,11 +5,11 @@
       :key="category.id"
       class="mb-8 w-1/2 borderrounded-md p-8"
     >
-      <h2 class="text-3xl font-bold">
+      <h2 class="text-4xl font-bold">
         {{ category.name[locale] }}
       </h2>
       <!-- <hr class="mb-12 mt-2 border"/> -->
-      <ul class="grid grid-cols-3 gap-16 border-2 rounded px-4 py-10 mt-4">
+      <ul class="grid grid-cols-3 gap-16 border-2 rounded px-6 py-10 mt-2">
         <li
           v-for="skill in category.skills"
           :key="skill.id"
@@ -18,13 +18,16 @@
           <p class="text-xl mb-2">
             {{ skill.name[locale] }}
           </p>
-          <div
-            class="h-4 bg-white rounded-full"
-            :style="{
-              width: `${(skill.level / 6) * 100}%`,
-              background: getBarGradient(skill.level)
-            }"
-          />
+          <div class="relative h-4 w-full">
+            <div
+              class="h-4 bg-white rounded-full absolute top-0 left-0 z-10"
+              :style="{
+                width: `${(skill.level / 6) * 100}%`,
+                background: getBarGradient(skill.level)
+              }"
+            />
+            <div class="h-4 border-1 rounded-full relative top-0 z-0 w-full"/>
+          </div>
         </li>
       </ul>
     </div>
